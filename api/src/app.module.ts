1ce07aacf8c7b9fd88d30f70bb5c0efad1b50034
Alexandre Devos <a.devos@octocorn.fr>
2024-04-20 12:59:44 -0400

    feat(tasks): Basic CRUD :sparkles:

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as process from 'node:process';
+import { Task } from './tasks/entities/task.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TasksModule,
+    ConfigModule.forRoot({
+      envFilePath: ['.env', '.env.local'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.HOST,
+      port: parseInt(process.env.PORT) || 3306,
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      entities: [Task],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
